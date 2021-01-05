083a9f7c027594901f0d6ce8e777b7b189515e1d
PonceHumberto <hbeto.pc@hotmail.com>
2021-01-05 18:41:55 -0500

    [FEATURE] Scaffol del Ejercicio9
    Agrega una nueva área para el desarrollo del ejercicio9

---
+++ Semana3/Areas/Ejercicio9/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Semana3.Areas.Ejercicio9.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Ejercicio9/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Semana3/Areas/Ejercicio9/Ejercicio9AreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace Semana3.Areas.Ejercicio9
+{
+    public class Ejercicio9AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Ejercicio9";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Ejercicio9_default",
+                "Ejercicio9/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "Semana3.Areas.Ejercicio9.Controllers" }
+
+            );
+        }
+    }
+}
