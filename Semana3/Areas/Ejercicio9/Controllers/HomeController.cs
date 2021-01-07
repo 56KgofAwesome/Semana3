98bd43daabbf21b4d18d21c295a11de76973f4d0
PonceHumberto <hbeto.pc@hotmail.com>
2021-01-06 19:00:38 -0500

    [FEATURE] Ejercicio9

--- Semana3/Areas/Ejercicio9/Controllers/HomeController.cs
+++ Semana3/Areas/Ejercicio9/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +12,47 @@ namespace Semana3.Areas.Ejercicio9.Controllers
         // GET: Ejercicio9/Home
         public ActionResult Index()
         {
+            return RedirectToAction("Create");
+        }
+        //GET
+        public ActionResult Create()
+        {
             return View();
         }
+        //Recepción de los datos
+        [HttpPost]
+        public ActionResult CreateCard(Card card)
+        {   
+            //Datos de la vista
+            ViewBag.name = card.name;
+            ViewBag.addressOne = card.suburb + ", " + card.street + ", " + card.apartment;
+            ViewBag.addressTwo = card.state + ", " + card.city + ", " + card.postalCode;
+            ViewBag.phoneNumber = card.phoneNumber;
+            ViewBag.email = card.email;
+            //Datos de la ruta con imagen aleatoria
+            string[] filePaths;            
+            if (card.path == "1")
+            {   
+                filePaths = Directory.GetFiles(@"C:\Users\Dev14\source\repos\Semana3\Semana3\Areas\Ejercicio9\Images\Enterprise\", "*.jpg");
+            }
+            else
+            {
+                filePaths = Directory.GetFiles(@"C:\Users\Dev14\source\repos\Semana3\Semana3\Areas\Ejercicio9\Images\Casual\", "*.jpg");
+            }
+            Random rnd = new Random();
+            int number = rnd.Next(1,3);
+            string randomImage = filePaths[number];
+            
+            ViewBag.src = randomImage;
+
+            return View();
+        }
+
+        //View de la carta de presentación
+        public ActionResult Card()
+        {   
+            return View();
+        }
+
     }
 }
---
+++ Semana3/Areas/Ejercicio9/Models/Card.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Semana3.Areas.Ejercicio9
+{
+    public class Card
+    {
+        [Required(ErrorMessage = "El {0} es requerido")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Nombre")]
+        public string name { set; get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Departamento")]
+        public string apartment { set; get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Calle")]
+        public string street { set; get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Colonia")]
+        public string suburb { set; get; }
+
+        [Required]
+        [DataType(DataType.PostalCode)]
+        [StringLength(5)]
+        [Display(Name = "Código Postal")]
+        public string postalCode { set; get; }
+
+        [Required]
+        [Display(Name = "Ciudad")]
+        public string city { set; get; }
+
+        [Required]
+        [Display(Name = "Estado")]
+        public string state { set; get; }
+
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Teléfono")]
+        public string phoneNumber { set; get; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Correo electrónico")]
+        public string email { set; get; }
+
+        [Required]
+        [Display(Name = "Catálogo de imágenes")]
+        public string path { set; get; }
+    }
+}
